02737c15b8d5ee17d29109a97a2a77c00fa9a3eb
Sam Samskies <samsamskies@gmail.com>
2021-12-25 19:41:02 -0600

    make location links shareable

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import ReactMapGL, { Popup } from "react-map-gl";
 import Head from "next/head";
 import QRCode from "qrcode.react";
@@ -7,6 +8,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const router = useRouter();
   const [viewport, setViewport] = useState({
     longitude: -89.4417,
     latitude: 13.4975,
@@ -15,17 +17,43 @@ export default function Home() {
     bearing: 0,
   });
   const [tooltip, setTooltip] = useState(null);
+  const handleViewportChange = (viewport) => {
+    if (router.isReady) {
+      router.query.long = viewport.longitude;
+      router.query.lat = viewport.latitude;
+      router.query.zoom = viewport.zoom;
+      router.push(router);
+    }
+
+    setViewport(viewport);
+  };
   const handleMapClick = ({ lngLat: [lng, lat], features }) => {
     const treasure = features.find(
       ({ sourceLayer }) => sourceLayer === "Bitcoin_Treasure_Map_4"
     );
-    console.log("treasure", treasure);
     const tooltip = treasure
       ? { value: treasure.properties.value, lat, lng }
       : null;
 
     setTooltip(tooltip);
   };
+
+  useEffect(() => {
+    if (router.isReady) {
+      const normalizeQueryParam = (param) => {
+        const normalized = Number(param);
+
+        return Number.isNaN(normalized) ? undefined : normalized;
+      };
+
+      setViewport({
+        ...viewport,
+        longitude: normalizeQueryParam(router.query?.long),
+        latitude: normalizeQueryParam(router.query?.lat),
+        zoom: normalizeQueryParam(router.query?.zoom),
+      });
+    }
+  }, [router.isReady]);
 
   return (
     <div>
@@ -39,7 +67,7 @@ export default function Home() {
         width="100vw"
         height="100vh"
         mapboxApiAccessToken="pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNrZ2Z0N2RoeTFvNDUyeXF1MmplaTF0b3oifQ.cJFNRdlJrym41VBUFq4aBQ"
-        onViewportChange={setViewport}
+        onViewportChange={handleViewportChange}
         onClick={handleMapClick}
       >
         <Info />
