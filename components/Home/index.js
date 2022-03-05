9f4f83928ff0dbd2ad0317d442a346fe9d14e2ce
Sam Samskies <samsamskies@gmail.com>
2022-03-05 15:16:44 -0600

    add geocoder

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useRouter } from "next/router";
 import ReactMapGL from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 import Head from "next/head";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNrZ2Z0N2RoeTFvNDUyeXF1MmplaTF0b3oifQ.cJFNRdlJrym41VBUFq4aBQ";
 
 export default function Home() {
   const router = useRouter();
+  const mapRef = useRef();
   const [viewport, setViewport] = useState({
     longitude: -89.4417,
     latitude: 13.4975,
@@ -13,16 +19,30 @@ export default function Home() {
     pitch: 0,
     bearing: 0,
   });
-  const handleViewportChange = (viewport) => {
-    if (router.isReady) {
-      router.query.long = viewport.longitude;
-      router.query.lat = viewport.latitude;
-      router.query.zoom = viewport.zoom;
-      router.push(router);
-    }
+  const handleViewportChange = useCallback(
+    (viewport) => {
+      if (router.isReady) {
+        router.query.long = viewport.longitude;
+        router.query.lat = viewport.latitude;
+        router.query.zoom = viewport.zoom;
+        router.push(router);
+      }
 
-    setViewport(viewport);
-  };
+      setViewport(viewport);
+    },
+    [router.isReady]
+  );
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides,
+      });
+    },
+    [handleViewportChange]
+  );
 
   useEffect(() => {
     if (router.isReady) {
@@ -48,13 +68,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ReactMapGL
+        ref={mapRef}
         {...viewport}
         mapStyle="mapbox://styles/smiyakawa/ckxm947bt40jf14oa5koel25y"
         width="100vw"
         height="100vh"
-        mapboxApiAccessToken="pk.eyJ1Ijoic21peWFrYXdhIiwiYSI6ImNrZ2Z0N2RoeTFvNDUyeXF1MmplaTF0b3oifQ.cJFNRdlJrym41VBUFq4aBQ"
+        mapboxApiAccessToken={MAPBOX_TOKEN}
         onViewportChange={handleViewportChange}
-      />
+      >
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </ReactMapGL>
     </div>
   );
 }
