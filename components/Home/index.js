943a1b52d23b36aebd873a976a9c3c7291384730
Sam Samskies <samsamskies@gmail.com>
2022-03-05 16:33:35 -0600

    zoom in closer to El Salvador on page load

--- components/Home/index.js
+++ components/Home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   const [viewport, setViewport] = useState({
     longitude: -89.4417,
     latitude: 13.4975,
-    zoom: 5,
+    zoom: 10,
     pitch: 0,
     bearing: 0,
   });
