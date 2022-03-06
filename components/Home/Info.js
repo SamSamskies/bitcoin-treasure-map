8da8bf911307318f5f067d1881243309f776ab97
Sam Samskies <samsamskies@gmail.com>
2022-03-05 19:53:45 -0600

    shrink app info on page load

--- components/Home/Info.js
+++ components/Home/Info.js
@@ -7,7 +7,7 @@ import InfoIcon from "@material-ui/icons/Info";
 import styles from "./Info.module.css";
 
 export default function Info() {
-  const [isInfoMaximized, setIsInfoMaximized] = useState(true);
+  const [isInfoMaximized, setIsInfoMaximized] = useState(false);
 
   return (
     <>
